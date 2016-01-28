26f0d829b0e809014632921029ce226a64384032
Thomas Månsson <thomasmonsson@gmail.com>
2016-01-28 14:07:12 +0100

    Initial Commit

---
+++ ColorMixer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorMixer
+//
+//  Created by Thomas on 2016-01-28.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorMixer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorMixer
+//
+//  Created by Thomas on 2016-01-28.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
